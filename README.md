14ab5c21b38b7d280fb22f3bf3814a487ee2f869
Brandon Shankar <bshankar12@comcast.net>
2019-10-19 23:36:56 -0700

    added to the js, html, and readme files

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # unit-4-game
+
+## Table of Contents
+* Introduction
+* Interesting bits of code
+* Technologies Used
+* How to use
+
+
+## Introduction
+In this program, there are four crystals and each of them have different values. The object of the game is to click on the crystals and reach the exact score of the number that the computer has generated. If the score is reached exactly, you win. However, if you are to go passed the score, you lose. 
+
+## Bits of code
+
+* This was used to print out the values of wins, losses, and total score onto the DOM or onto the wbpage itself
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var totalScoreText = document.getElementById("total-score-text");
+
+* This is code in jquery that is used to demonstrate that the image of the id crystal is to be clicked for this function to take place
+ $(".crystal-image").on("click", function() {
+	
+## Technologies
+Project is created using the following:
+* HTML
+* CSS
+* Javascript
+* JQuery
+
+# How to use
+ Begin by clicking on a crystal and checking out what value it has. Then click on other crystals in order to reach the score of the computer generated number that appears on the screen.
+
+
+	
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,7 @@
 //}
 
 //create the random number that is to be reached by the guesses
-  var targetNumber = Math.floor(Math.random() * 19 + 120);
+  var targetNumber = Math.floor(Math.random() * 19 + 101);
 
   $("#number-to-guess").text(targetNumber);
 
@@ -30,34 +30,34 @@
 
     // images of the crystals using jQuery to tag images in
     var imageCrystal1 = $("<img>");
-    var imageCrystal2 = $("<img>");
-    var imageCrystal3 = $("<img>");
-    var imageCrystal4 = $("<img>");
+    // var imageCrystal2 = $("<img>");
+    // var imageCrystal3 = $("<img>");
+    // var imageCrystal4 = $("<img>");
 
     // add the crystals to the crystal class to then show up on screen
     
     imageCrystal1.addClass("crystal-image");
-    imageCrystal2.addClass("crystal-image");
-    imageCrystal3.addClass("crystal-image");
-    imageCrystal4.addClass("crystal-image");
+    // imageCrystal2.addClass("crystal-image");
+    // imageCrystal3.addClass("crystal-image");
+    // imageCrystal4.addClass("crystal-image");
 
     // Link in the actual images of the four crystals
     imageCrystal1.attr("src", "assets/images/Dragon.png");
-    imageCrystal2.attr("src", "assets/images/Fire.png");
-    imageCrystal3.attr("src", "assets/images/Grass.png");
-    imageCrystal4.attr("src", "assets/images/Water.png");
+    // imageCrystal2.attr("src", "assets/images/Fire.png");
+    // imageCrystal3.attr("src", "assets/images/Grass.png");
+    // imageCrystal4.attr("src", "assets/images/Water.png");
 
     // the imageCrystals will have the set number data linked to them respectively from the crystalNumbers array
     imageCrystal1.attr("data-crystalvalue", crystalNumbers[i]);
-    imageCrystal2.attr("data-crystalvalue", crystalNumbers[i]);
-    imageCrystal3.attr("data-crystalvalue", crystalNumbers[i]);
-    imageCrystal4.attr("data-crystalvalue", crystalNumbers[i]);
+    // imageCrystal2.attr("data-crystalvalue", crystalNumbers[i]);
+    // imageCrystal3.attr("data-crystalvalue", crystalNumbers[i]);
+    // imageCrystal4.attr("data-crystalvalue", crystalNumbers[i]);
 
     // the crystals will be present on the page/ appended to the webpage
     $("#crystals").append(imageCrystal1);
-    $("#crystals").append(imageCrystal2);
-    $("#crystals").append(imageCrystal3);
-    $("#crystals").append(imageCrystal4);
+    // $("#crystals").append(imageCrystal2);
+    // $("#crystals").append(imageCrystal3);
+    // $("#crystals").append(imageCrystal4);
   }
 
   // need to click on a crystal
@@ -68,8 +68,8 @@
     // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
     // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
 
-    // var crystalValue = ($(this).attr("data-crystalvalue"));
-    // crystalValue = parseInt(crystalValue);
+     var crystalValue = ($(imageCrystal1).attr("data-crystalvalue"));
+     crystalValue = parseInt(crystalValue);
     // We then add the crystalValue to the user's "counter" which is a global variable.
     // Every click, from every crystal adds to the global counter.
     counter += crystalValue;
